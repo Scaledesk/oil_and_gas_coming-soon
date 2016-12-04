5ef09aa488753db0ea60eb7bdf879513d1247a5f
sanchitrathee <sanchit2411@gmail.com>
2016-12-04 18:05:50 +0530

    gallery fix and new images

--- js/main.js
+++ js/main.js
@@ -195,17 +195,17 @@ $(document).ready(function(){
     /* 5. Newsletter ........................ */
     /* ------------------------------------- */
 
-    $("#notifyMe").notifyMe();
-
-    (function() {
-
-        var dlgtrigger = document.querySelector( '[data-dialog]' ),
-            somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
-            dlg = new DialogFx( somedialog );
-
-        dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
-
-    })();
+//    $("#notifyMe").notifyMe();
+//
+//    (function() {
+//
+//        var dlgtrigger = document.querySelector( '[data-dialog]' ),
+//            somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
+//            dlg = new DialogFx( somedialog );
+//
+//        dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
+//
+//    })();
 
     /* ------------------------------------- */
     /* 6. PhotoSwipe Gallery Init .......... */
